252dd459d2d9774033000ab9a745c8e26f2905e5
github-action-benchmark <github@users.noreply.github.com>
2024-03-01 13:54:17 +0000

    add Extraction algorithm Benchmark for inband data (benchmarkjs) benchmark result for 855b1019a8d3800deec761cfe9a05a1b19692bf5

---
+++ docs/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1709301257158,
+  "repoUrl": "https://github.com/xdxxxdx/ldes-client",
+  "entries": {
+    "Extraction algorithm Benchmark for inband data": [
+      {
+        "commit": {
+          "author": {
+            "email": "xueying_deng@outlook.com",
+            "name": "XD",
+            "username": "xdxxxdx"
+          },
+          "committer": {
+            "email": "xueying_deng@outlook.com",
+            "name": "XD",
+            "username": "xdxxxdx"
+          },
+          "distinct": true,
+          "id": "855b1019a8d3800deec761cfe9a05a1b19692bf5",
+          "message": "feat: perf",
+          "timestamp": "2024-03-01T14:41:59+01:00",
+          "tree_id": "0255a05518947419ad6c08eb35cde38a8ec724dd",
+          "url": "https://github.com/xdxxxdx/ldes-client/commit/855b1019a8d3800deec761cfe9a05a1b19692bf5"
+        },
+        "date": 1709301256167,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "descending tree, emits ordered",
+            "value": 547380,
+            "range": "±23.09%",
+            "unit": "ops/sec",
+            "extra": "60 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
